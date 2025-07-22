0bd43df331173abf8bf60667a327840bab1aed6e
ParkYuGeun <yg35454906@gmail.com>
2025-07-23 03:43:47 +0900

    default setting & test

---
+++ VR_prac/Assets/Script/EventTest.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFirstHoverEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFirstHoverEntered");
+    }
+
+    public void OnLastHoverExited()
+    {
+        Debug.Log($"{gameObject.name} - OnLastHoverExited");
+    }
+
+    public void OnHoverEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnHoverEntered");
+    }
+
+    public void OnHoverExited()
+    {
+        Debug.Log($"{gameObject.name} - OnHoverExited");
+    }
+
+    public void OnFirstSelectEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnFirstSelectEntered");
+    }
+
+    public void OnLastSelectExited()
+    {
+        Debug.Log($"{gameObject.name} - OnLastSelectExited");
+    }
+
+    public void OnSelectEntered()
+    {
+        Debug.Log($"{gameObject.name} - OnSelectEntered");
+    }
+
+    public void OnSelectExited()
+    {
+        Debug.Log($"{gameObject.name} - OnSelectExited");
+    }
+
+    public void OnActivated()
+    {
+        Debug.Log($"{gameObject.name} - OnActivated");
+    }
+
+    public void OnDeactivated()
+    {
+        Debug.Log($"{gameObject.name} - OnDeactivated");
+    }
+}
+
