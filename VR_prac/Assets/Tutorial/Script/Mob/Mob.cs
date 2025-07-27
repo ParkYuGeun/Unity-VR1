59a8158e66aa08c8504adcd932cb3080968858ed
ParkYuGeun <yg35454906@gmail.com>
2025-07-28 04:33:44 +0900

    2025.07.28

---
+++ VR_prac/Assets/Tutorial/Script/Mob/Mob.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mob : MonoBehaviour
+{
+    public float huemin = 0f;
+    public float huemax = 1f;
+    public float saturationMin = 0.7f;
+    public float saturationMax = 1f;
+    public float valueMin = 0.7f;
+    public float valueMax = 1f;
+
+    public float arrangeRange = 0.5f;
+
+    public float emissionIntensity = 5f;
+
+    public ParticleSystem environmentParticle;
+    public MeshRenderer holeMeshRenderer;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        agent.SetDestination(new Vector3(0f,2f,1f));
+        agent.speed *= Random.Range(0.8f, 1.5f);
+
+        RandomColor();
+    }
+
+    private void RandomColor() 
+    { 
+        var color = Random.ColorHSV(huemin,huemax, saturationMin, saturationMax,valueMin,valueMax);
+
+        var main = environmentParticle.main;
+        main.startColor = new ParticleSystem.MinMaxGradient(color, color * Random.Range(1f - arrangeRange, 1f + arrangeRange));
+
+        var renderer = environmentParticle.GetComponent<ParticleSystemRenderer>();
+        renderer.material.SetColor("_EmissionColor", color * emissionIntensity);
+
+       holeMeshRenderer.material.SetColor("_EmissionColor", color * emissionIntensity);
+    }
+
+}
---
+++ VR_prac/Assets/Tutorial/Script/Mob/Spawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prepab;
+
+    public bool playOnstart = true;
+    public float startFactor = 1f;
+    public float additiveFactor = 0.1f;
+    public float delayperSpawnGroup = 3f;
+
+    private void Start()
+    {
+        if (playOnstart = true)
+            Play();
+    }
+
+    public void Play()
+    {
+        StartCoroutine(Process());
+    }
+
+    public void Stop()
+    {
+        StopAllCoroutines();
+    }
+
+    public IEnumerator Process()
+    {
+        var factor = startFactor;
+        var wfs = new WaitForSeconds(delayperSpawnGroup);
+
+        while (true)
+        {
+            yield return wfs;
+            yield return StartCoroutine(SpawnProcess(factor));
+            factor += additiveFactor;
+        }
+    }
+
+    private IEnumerator SpawnProcess(float factor)
+    {
+        var count = Random.Range(factor, factor * 2f);
+
+        for(int i = 0; i < count; i++)
+        {
+            Spawn();
+            if(Random.value <0.2f)
+                yield return new WaitForSeconds(Random.Range(0.01f,0.02f));
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(prepab, transform.position, transform.rotation, transform);
+    }
+}
